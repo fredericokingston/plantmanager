8f56d79e2dc5d4705fea6db141c3362498c6aff7
Frederico Velho <fredericovcoelho@gmail.com>
2021-05-03 15:24:58 -0300

    PlantSelect FlatList refactoring

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -117,6 +117,7 @@ export function PlantSelect() {
         <View >
           <FlatList
             data={enviroments}
+            keyExtractor={(item) => String(item.key)}
             renderItem={({item}) =>
               <EnvironmentButton
                 title={item.title}
@@ -133,6 +134,7 @@ export function PlantSelect() {
         <View style={styles.plantList}>
           <FlatList
             data={filteredPlants}
+            keyExtractor={(item) => String(item.id)}
             renderItem={({ item }) => (
               <PlantCardPrimary
                 data={item}
