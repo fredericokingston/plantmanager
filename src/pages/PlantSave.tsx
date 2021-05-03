7fd0478bb66c24e70518822ef9403fb9e2fe731e
Frederico Velho <fredericovcoelho@gmail.com>
2021-05-03 19:21:39 -0300

    PlantProps refactoring

--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -13,27 +13,17 @@ import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { SvgFromUri } from 'react-native-svg'
 import { useRoute } from '@react-navigation/core';
 import DateTimePicker, { Event } from '@react-native-community/datetimepicker'
+import { format, isBefore } from 'date-fns';
 
 import waterDropImg from '../assets/waterdrop.png'
 import { Button } from '../components/Button';
+import { PlantProps } from '../libs/storage';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-import { format, isBefore } from 'date-fns';
 
 interface Params {
-  plant: {
-    id: string;
-    name: string;
-    about: string;
-    water_tips: string;
-    photo: string;
-    environments: [string];
-    frequency: {
-      times: number;
-      repeat_every: string;
-    }
-  }
+  plant: PlantProps;
 }
 
 export function PlantSave() {
