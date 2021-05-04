02c634eabadda3b0092cbfe9833ebc676d2b7017
Frederico Velho <fredericovcoelho@gmail.com>
2021-05-04 10:50:50 -0300

    tab navigation

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,11 +4,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
 
 import colors from '../styles/colors';
 import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -38,7 +38,7 @@ const AppRoutes: React.FC = () => (
 
     <stackRoutes.Screen
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
     />
     <stackRoutes.Screen
       name="PlantSave"
@@ -46,7 +46,7 @@ const AppRoutes: React.FC = () => (
     />
     <stackRoutes.Screen
       name="MyPlants"
-      component={MyPlants}
+      component={AuthRoutes}
     />
 
   </stackRoutes.Navigator>
